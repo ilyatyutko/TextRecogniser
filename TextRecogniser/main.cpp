fc9b4bd09af5955f2bf0a8795afd8ba554e757c2
ilyatyutko <46351921+ilyatyutko@users.noreply.github.com>
2020-07-18 17:53:23 +0300

    Before trying MultyThreading

--- TextRecogniser/main.cpp
+++ TextRecogniser/main.cpp
@@ -1,19 +1,24 @@
 #include <bitset>
 #include <iostream>
+#include <ctime>
 #include "include/DigitRecogniser.h"
 
 int main()
 {
-	std::cout << "1:" << DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample1.png")) << std::endl;
-	std::cout << "2:" << DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample2.png")) << std::endl;
-	std::cout << "3:" << DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample3.png")) << std::endl;
-	std::cout << "4:" << DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample4.png")) << std::endl;
-	std::cout << "5:" << DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample5.png")) << std::endl;
-	std::cout << "6:" << DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample6.png")) << std::endl;
-	std::cout << "7:" << DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample7.png")) << std::endl;
-	std::cout << "8:" << DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample8.png")) << std::endl;
-	std::cout << "9:" << DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample9.png")) << std::endl;
-	std::cout << "0:" << DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample0.png")) << std::endl;
-
+	unsigned long long begin = clock();
+	for (int i = 0; i < 1000; ++i)
+	{
+		DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample1.png"));
+		DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample2.png"));
+		DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample3.png"));
+		DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample4.png"));
+		DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample5.png"));
+		DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample6.png"));
+		DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample7.png"));
+		DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample8.png"));
+		DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample9.png"));
+		DigitRecogniser::RecogniseDigit(std::string("SampleImages/sample0.png"));
+	}
+	std::cout << (clock() - begin) / 1000  << "  SEC";
 	return 0;
 }
