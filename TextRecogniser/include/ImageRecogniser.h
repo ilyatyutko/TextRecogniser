6fbd1af9222dc8224780d73b1d06ac16eee07aa4
ilyatyutko <46351921+ilyatyutko@users.noreply.github.com>
2020-07-31 15:35:43 +0300

    AGAIN WRONG

--- TextRecogniser/include/ImageRecogniser.h
+++ TextRecogniser/include/ImageRecogniser.h
@@ -15,27 +15,25 @@ using FileName = std::string;
 class ImageRecogniser
 {
 private:
-	Hopfild<int> NeuralNet;
+	Hopfild<char> NeuralNet;
 public:
-	ImageRecogniser(const std::list<std::pair<FileName, int>>& FileList)
+	ImageRecogniser(const std::list<std::pair<FileName, char>>& FileList)
 	{
 		if (FileList.size() == 0)
 			throw std::exception("Empty Input List");
 
-		std::list<std::pair<std::vector<bool>, int>> Images;
+		std::list<std::pair<std::vector<bool>, char>> Images;
 
 		for (auto& file : FileList)
 		{
-			//ImageTransformer picture(file.first);
-			auto SymbolToLearn = InputImageCutter::CutImage(file.first).front();
-			auto tmp = SymbolToLearn.GetBlackAndWhiteVector();
 			Images.push_back( 
-				std::make_pair(tmp
+				std::make_pair(
+					InputImageCutter::CutImage(file.first).front().GetBlackAndWhiteVector()
 							   , file.second));
 		}
 
-		//there 10 is element, which means impossibility to recognise
-		NeuralNet = Hopfild<int>(Images, 10); 
+		//there '\a' is element, which means impossibility to recognise
+		NeuralNet = Hopfild<char>(Images, '\a'); 
 	}
 	int RecognizeImage(const Figure& BinaryImage) const
 	{
